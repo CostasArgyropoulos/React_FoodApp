d4bb637de6b209a13afc259ba3fc229c4802ce19
Kargyrop <kargyr.ankronet@gmail.com>
2022-02-28 00:54:42 +0200

    FoorApp. NO LOGIC

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+    return <li>
+        <MealsSummary />
+        <AvailableMeals />
+    </li>
+};
+
+export default Meals;
